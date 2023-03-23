f3b773285cb5a87ecab3f90d5ba5aa0ed40413c2
evan <lyh10856214@qq.com>
2023-03-24 02:57:31 +0800

    feat():新增httpClient demo

---
+++ httpDemo/demo1/goHttpClient.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("http.Get err:", err)
+		return
+	}
+	//print base info
+
+	fmt.Println("Header:", resp.Header)
+	fmt.Println("Proto:", resp.Proto)
+	fmt.Println("Status:", resp.Status)
+	fmt.Println("ContentLength:", resp.ContentLength)
+	//关闭内容流
+	defer resp.Body.Close()
+
+	buf := make([]byte, 4098)
+	var content string
+	for {
+		n, err := resp.Body.Read(buf)
+		if err != nil {
+			break
+		}
+		if n == 0 {
+			fmt.Println("Finished")
+			break
+		}
+		content += string(buf)
+	}
+
+	fmt.Printf("|%v|\n", content)
+}
