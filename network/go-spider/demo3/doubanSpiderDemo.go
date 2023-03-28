4ee8a6cc434253db43350e28406c6fa7b9af28ec
evan <lyh10856214@qq.com>
2023-03-29 02:22:26 +0800

    feat():添加豆瓣爬虫demo

---
+++ network/go-spider/demo3/doubanSpiderDemo.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+func main() {
+	//输入页数
+	var start, end int
+
+	fmt.Println("请输入起始页:")
+	fmt.Scan(&start)
+	fmt.Println("请输入尾页:")
+	fmt.Scan(&end)
+
+	//处理任务
+	doWork(start, end)
+}
+
+func doWork(start int, end int) {
+	channel := make(chan int)
+	//根据页面遍历远程爬取页面
+	for i := start; i <= end; i++ {
+		go crawlDB(i, channel)
+
+	}
+	//监听任务处理结束状态
+	for i := start; i <= end; i++ {
+		fmt.Printf("第%d页已经完成\n", <-channel)
+	}
+}
+
+func crawlDB(page int, channel chan int) {
+
+	//远程请求豆瓣网页，并获取返回值
+	url := "https://movie.douban.com/top250?start=" + strconv.Itoa((page-1)*25) + "&filter="
+	time.Sleep(1 * time.Second)
+	result, err := HttpGet(url)
+	if err != nil {
+		fmt.Println("HttpGet err:", err)
+		return
+	}
+
+	//生成文件
+	fileName := "第" + strconv.Itoa(page) + "页电影榜.txt"
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("os.Create err:", err)
+		return
+	}
+	//解析返回结果
+	fmt.Println("body:", result)
+	//电影名
+	titleRegex := regexp.MustCompile("<img width=\"100\" alt=\"(.*?)\" src=\"(.*?)\" class=\"\">")
+	titles := titleRegex.FindAllStringSubmatch(result, -1)
+
+	scoreRegex := regexp.MustCompile("<span class=\"rating_num\" property=\"v:average\">(.*?)</span>")
+	scores := scoreRegex.FindAllStringSubmatch(result, -1)
+
+	pplCountRegex := regexp.MustCompile("<span>(.*?)人评价</span>")
+	pplCount := pplCountRegex.FindAllStringSubmatch(result, -1)
+
+	file.WriteString("Tile|score|pplCount")
+	fmt.Println("title count:", len(titles))
+	for i := 0; i < len(titles); i++ {
+		file.WriteString(titles[i][1] + "|" + scores[i][1] + "|" + pplCount[i][1] + "\n")
+	}
+	file.Close()
+	//输出任务结束状态
+	channel <- page
+
+}
+
+func HttpGet(url string) (result string, err error) {
+	fmt.Println("url:", url)
+	resp, httpErr := http.Get(url)
+	if httpErr != nil {
+		fmt.Println("http.Get err:", err)
+		err = httpErr
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Printf("body:%v", resp)
+	//读取响应体
+	buf := make([]byte, 4098)
+	for {
+
+		n, readErr := resp.Body.Read(buf)
+		fmt.Println("n:", n)
+		if n == 0 {
+			break
+		}
+		if readErr != nil && readErr != io.EOF {
+			fmt.Println("请求网页异常:", readErr)
+			err = readErr
+			break
+		}
+		result += string(buf[:n])
+		fmt.Println("result:", result)
+	}
+	return result, err
+}
